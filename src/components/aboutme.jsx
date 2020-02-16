1fcf8c68491f03bdad087d95e1d4cbfe95f65155
denyshubh <denyshubham@gmail.com>
2020-02-16 18:53:04 +0530

    edited about us page

--- src/components/aboutme.jsx
+++ src/components/aboutme.jsx
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 
 class About extends Component {
   render() {
-    return(
-      <div><h1>About Page</h1></div>
+    return (
+      <div><h1>Hii This is Shubham Singh</h1></div>
     )
   }
 }
