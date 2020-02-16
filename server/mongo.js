55a3afb00f3c1e042cca05bb820db126a3f027ab
denyshubh <denyshubham@gmail.com>
2020-02-16 15:16:09 +0530

    added cosmos db credentials

--- server/mongo.js
+++ server/mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-const env = require('./env/environment');
-const key = env.key
+const env = require('../config');
+const key = env.authKey
 const dbName = env.dbName
-const port = env.cosmosPort
+const port = env.port
 
 mongoose.Promise = global.Promise;
 const mongoUri = `mongodb://${dbName}:${key}@${dbName}.documents.azure.com:${port}/?ssl=true&replicaSet=globaldb`;
