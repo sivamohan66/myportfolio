3f593cd2b3beeec641e5816797a7cca86c5ec130
denyshubh <denyshubham@gmail.com>
2020-04-01 17:12:36 +0530

    Updated Landing page This update contains the new image icon, a card view for show the developer and skills. Improved code by using map and removing multiple same line of code. This would help us when getting data from server.

--- client/src/components/landingpage.jsx
+++ client/src/components/landingpage.jsx
@@ -4,41 +4,50 @@ import ProfileImage from './profileImage';
 
 class Landing extends Component {
 
+  skills = (skillList = []) => {
+    return (
+      <div className="row justify-content-center m-2">
+        <div className="col-10">
+          {
+            skillList.map((skill, index) => <p className="card-text badge badge-primary text-wrap m-2 p-2" key={index}>{skill}</p>)
+          }
+        </div>
+      </div>
+    )
+  }
+
+  socialLinks = (urls = []) => {
+    return (
+      <div className="social-links">
+        {
+          Object.keys(urls).map((url, index) =>
+            <a href={url} rel="noopener noreferrer" target="_blank" key={index}>
+              <i className={urls[url]} aria-hidden="true" />
+            </a>
+          )
+        }
+      </div>
+    )
+  }
+
   details = () => {
     return (
       <section>
         <div className="card text-center">
           <div className="card-header">
-            <h1 className="text-center text-capitalize font-weight-bold title">Full Stack Web Developer</h1>
+            <h1 className="text-center text-capitalize font-weight-bold title text-primary">Full Stack Web Developer</h1>
           </div>
           <div className="card-body">
-            <h5 className="card-title">My Skills</h5>
-            <p className="card-text">HTML/CSS | Bootstrap | JavaScript | React | React Native | NodeJS | Express | MongoDB</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
+            <h3 className="card-title font-weight-bold">My Skills</h3>
+            {this.skills(["React", "Redux", "System Admin", "Cloud Computing", "Bootstrap", "Python"])}
           </div>
           <div className="card-footer text-muted">
-            <div className="social-links">
-
-              {/* LinkedIn */}
-              <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-                <i className="fa fa-linkedin-square" aria-hidden="true" />
-              </a>
-
-              {/* Github */}
-              <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-                <i className="fa fa-github-square" aria-hidden="true" />
-              </a>
-
-              {/* Freecodecamp */}
-              <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-                <i className="fa fa-free-code-camp" aria-hidden="true" />
-              </a>
-
-              {/* Youtube */}
-              <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-                <i className="fa fa-youtube-square" aria-hidden="true" />
-              </a>
-            </div>
+            {this.socialLinks({
+              "https://www.linkedin.com/in/denyshubham/": "fa fa-linkedin-square",
+              "https://www.github.com/denyshubh": "fa fa-github-square",
+              "https://medium.com/@shubham.singh98": "fa fa-free-code-camp",
+              "http://google.com": "fa fa-youtube-square"
+            })}
           </div>
         </div>
       </section>
